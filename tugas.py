c6fcebf29646e83958366b43e2f1bb07c13e2420
ReskiqhaNurfatihaSariKelasA <ayaycaptain777@gmail.com>
2026-03-15 15:59:18 +0800

    Update tugas.py

--- tugas.py
+++ tugas.py
@@ -1,24 +1,33 @@
+#Library Request
 import requests
 
 response = requests.get("https://api.github.com")
 print(response.status_code)
 print(response.text)
 
+
+#Library Math 
 import math
 
 print(math.sqrt(99))
 print(math.pi)
 
+
+#Library Random
 import random
 
 angka = random.randint(3, 67)
 print("angka acak:", angka)
 
+
+#Library Datetime
 from datetime import datetime
 
 sekarang = datetime.now()
 print("Waktu sekarang:", sekarang)
 
+
+#Studi Kasus Sederhana
 import random
 
 nilai = random.randint(20, 70)
